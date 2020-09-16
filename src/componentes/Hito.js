f90377e627f0318d3fa222d9cfb36aec711591ce
lchaconc <luis.chacon.campos@gmail.com>
2020-09-16 09:13:25 -0600

    Initial commit

---
+++ src/componentes/Hito.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Hito () {
+
+    return (
+        <div className="row">
+            <div className="col-5">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis ut eos magni, cum ab possimus consectetur vero dolor voluptate, at quae ullam, in eaque nobis! Ducimus, id! Velit, dolore blanditiis.
+            </div>
+            <div className="col-2">
+                2020
+            </div>
+            <div className="col-5">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias delectus cum voluptatem ex dignissimos doloribus, deleniti aliquid non saepe voluptas voluptate tempora vero provident harum quisquam aperiam, excepturi velit temporibus.
+            </div>
+        </div>
+    )
+    
+}
+
+export default Hito;
---
+++ src/componentes/Main.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Hito from './Hito';
+
+
+function Main () {
+
+
+    return (
+        <React.Fragment>
+            <div className="jumbotron">
+            <h1>Línea de tiempo PNTM</h1>
+        </div>
+        <div className="container">
+            PRUEBA DE VISTAS
+             <hr/>
+             <Hito />
+        </div>
+        </React.Fragment>
+    )
+        
+    
+}
+
+export default Main;
