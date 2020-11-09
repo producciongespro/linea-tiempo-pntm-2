f3015c23fbb99be51f4696628c4f24125dcdf8a9
PatriciaHdz <32460268+patyhc4oct@users.noreply.github.com>
2020-11-09 12:36:17 -0600

    Pie de página y Acerca de
    Se hacen cambios necesarios para que se vean en el diseño para trasladar a Mariana Molina

--- src/componentes/Escritorio.js
+++ src/componentes/Escritorio.js
@@ -7,17 +7,35 @@ export default function Escritorio(props) {
 
     return (
         <React.Fragment>
-            <div className="jumbotron">
-                <h1>Antecedentes TDIC</h1>
+    <div className="jumbotron">
+        <div className="row"> 
+
+            <div className="col-10">
+                 <h1>Antecedentes TDIC</h1>
                 <h2>Hacia una ciudadanía digital con equidad social</h2>
                 <strong>
                     <span className="font-2" >
-                        Línea de Tiempo de la Inclusión de las Tecnologías Digitales en la Educación Costarricense
-            </span>
-                </strong>
-            </div>
-            <hr />
+                    Línea de Tiempo de la Inclusión de las Tecnologías Digitales en la Educación Costarricense
+                    </span>
+                </strong> 
+            </div> 
+
+            <div className="col-2 acerca">
+                <button>Acerca de</button>
+           </div>
+         </div>
+    </div><hr />
+
             <HitoEsc data={props.data} />
+
+           <div className="footer">
+            <div className="row">
+                <div className="col-12 pie">
+                <p>Dirección de Recursos Tecnológicos</p>  
+                </div>
+           </div>
+           </div>
+         
         </React.Fragment>
     )
 
